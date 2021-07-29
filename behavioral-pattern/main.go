376e4eda138f833d62a0d7d36017b51b5f2c5976
anandadwirahma <anandadwi20@gmail.com>
2021-07-29 17:04:24 +0700

    client command & iterator pattern

--- behavioral-pattern/main.go
+++ behavioral-pattern/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println("========== Chan Of Responsibilty Pattern ==========")
 	CallChainOfResponsibility()
+
+	fmt.Println("========== Command Pattern ==========")
+	CallCommandMethod()
+
+	fmt.Println("========== Iterator Pattern ==========")
+	CallIteratorMethod()
 }
